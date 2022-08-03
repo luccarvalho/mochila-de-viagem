3c07b8987e0d90136b1cbc42c490fa85e09ad588
Lucas Carvalho <102173672+luccarvalho@users.noreply.github.com>
2022-08-03 14:19:01 -0300

    Add files via upload
    Adicionando novas linhas de código JavaScript.

--- js/main.js
+++ js/main.js
@@ -1,25 +1,39 @@
+// Operador lógico que retorna com dados salvos, ou string vazia, utilizando localStorage.getItem,
+// modificando o valor de `string` com JSON.parse()
 const form = document.getElementById("novoItem");
 const lista = document.getElementById("lista");
 const itens = JSON.parse(localStorage.getItem("itens")) || [];
 
+// Uso do forEach para que todos os itens já escritos na lista sejam mantidos ao atualizar a página.
 itens.forEach((elemento) => {
     criaElemento(elemento);
 })
 
+// addEventListener para receber as funções extras da função criaElemento.
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
 
     const nome = evento.target.elements['nome'];
     const quantidade = evento.target.elements['quantidade'];
 
+    const existe = itens.find(elemento => elemento.nome === nome.value);
+
     const itemAtual = {
         "nome": nome.value,
         "quantidade": quantidade.value
     }
 
-    criaElemento(itemAtual);
+    if (existe) {
+        itemAtual.id = existe.id;
 
-    itens.push(itemAtual);
+        atualizaElemento(itemAtual);
+    } else {
+        itemAtual.id = itens.length;
+
+        criaElemento(itemAtual);
+
+        itens.push(itemAtual);
+    }
 
     localStorage.setItem("itens", JSON.stringify(itens));
 
@@ -27,17 +41,21 @@ form.addEventListener("submit", (evento) => {
     quantidade.value = ""
 });
 
+// função `criaElemento` para que possua apenas a função que faça sentido ao nome.
 function criaElemento(item) {
     const novoItem = document.createElement('li');
     novoItem.classList.add('item');
 
     const numeroItem = document.createElement('strong');
     numeroItem.innerHTML = item.quantidade;
+    numeroItem.dataset.id = item.id;
 
     novoItem.appendChild(numeroItem);
     novoItem.innerHTML += item.nome;
 
     lista.appendChild(novoItem);
+}
 
-
+function atualizaElemento(item) {
+    document.querySelector("[data-id='" + item.id + "']").innerHTML = item.quantidade;
 }
