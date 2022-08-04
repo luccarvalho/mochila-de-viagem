1c3e0d4f2e7a62004b06d8dd34261260dcd97d16
Lucas Carvalho <102173672+luccarvalho@users.noreply.github.com>
2022-08-04 12:06:29 -0300

    Add files via upload
    Adicionando funcionalidade com botão que deleta itens da lista, com JavaScript.

--- js/main.js
+++ js/main.js
@@ -57,9 +57,26 @@ function criaElemento(item) {
     novoItem.appendChild(numeroItem);
     novoItem.innerHTML += item.nome;
 
+    novoItem.appendChild(botaoDeleta());
+
     lista.appendChild(novoItem);
 }
 
 function atualizaElemento(item) {
     document.querySelector("[data-id='" + item.id + "']").innerHTML = item.quantidade;
+}
+
+function botaoDeleta() {
+    const elementoBotao = document.createElement("button");
+    elementoBotao.innerHTML = "X";
+
+    elementoBotao.addEventListener("click", function () {
+        deletaElemento(this.parentNode);
+    })
+
+    return elementoBotao;
+}
+
+function deletaElemento(tag) {
+    tag.remove()
 }
